6c18eb5d9d9e8493c146ea25313e3df6e9bb889c
huoshan017 <huoshan017@hotmail.com>
2019-06-19 18:40:02 +0800

    11111

--- sh_script/gen_db.sh
+++ sh_script/gen_db.sh
@@ -4,8 +4,8 @@ cd ../db_define
 mkdir -p proto
 
 cd ../tools
-code_generator -c ../db_define/login_db.json -d ../src/login_server -p ../db_define/proto/login_db.proto
+./code_generator -c ../db_define/login_db.json -d ../src/login_server -p ../db_define/proto/login_db.proto
 
 cd ../third_party/protobuf
-protoc --go_out=../../src/login_server/login_db --proto_path=../../db_define/proto login_db.proto
+./protoc --go_out=../../src/login_server/login_db --proto_path=../../db_define/proto login_db.proto
 cd ../../db_define
