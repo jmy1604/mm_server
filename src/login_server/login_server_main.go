3d69124790ff06a7397faeaefb113d21b6262c08
huoshan017 <huoshan017@hotmail.com>
2020-05-03 22:18:03 +0800

    111

--- src/login_server/login_server_main.go
+++ src/login_server/login_server_main.go
@@ -29,23 +29,23 @@ func main() {
 	}()
 
 	if !server_config.ServerConfigLoad("login_server.json", &config) {
-		fmt.Printf("载入LoginServer配置失败")
+		fmt.Printf("载入LoginServer配置失败\n")
 		return
 	}
 
-	log.Event("配置:服务器ID", config.ServerId)
-	log.Event("配置:服务器名称", config.ServerName)
-	log.Event("配置:服务器地址(对Client)", config.ListenClientIP)
-	log.Event("配置:服务器地址(对Game)", config.ListenGameIP)
+	fmt.Printf("配置:服务器ID\n", config.ServerId)
+	fmt.Printf("配置:服务器名称\n", config.ServerName)
+	fmt.Printf("配置:服务器地址(对Client)\n", config.ListenClientIP)
+	fmt.Printf("配置:服务器地址(对Game)\n", config.ListenGameIP)
 
 	server_list.ReadConfig(server_config.GetConfPathFile("server_list.json"))
 
 	if !global_config_load() {
-		log.Error("global_config_load failed !")
+		fmt.Printf("global_config_load failed !\n")
 		return
 	}
 
-	log.Event("连接数据库", config.MYSQL_NAME, log.Property{"地址", config.MYSQL_IP})
+	fmt.Printf("连接数据库\n", config.MYSQL_NAME, log.Property{"地址", config.MYSQL_IP})
 
 	var err error
 	if !db_use_new {
@@ -57,30 +57,30 @@ func main() {
 			}
 		}())
 		if err != nil {
-			log.Error("连接数据库失败 %v", err)
+			fmt.Printf("连接数据库失败 %v\n", err)
 			return
 		} else {
-			log.Event("连接数据库成功", nil)
+			fmt.Printf("连接数据库成功\n", nil)
 			go dbc.Loop()
 		}
 	} else {
 		/*if !db_new_init(config.DB_DEFINE) {
 			return
 		}*/
-		log.Trace("db new init success")
+		fmt.Printf("db new init success\n")
 	}
 
 	if !signal_mgr.Init() {
-		log.Error("signal_mgr init failed")
+		fmt.Printf("signal_mgr init failed\n")
 		return
 	}
 
 	if !db_use_new {
 		if nil != dbc.Preload() {
-			log.Error("dbc Preload Failed !!")
+			fmt.Printf("dbc Preload Failed !!\n")
 			return
 		} else {
-			log.Info("dbc Preload succeed !!")
+			fmt.Printf("dbc Preload succeed !!\n")
 		}
 	}
 
